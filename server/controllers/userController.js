ba299f5506e0d62cadec1c5de96620bd52a30183
Alexander Chentsov <2262659@cshawi.ca>
2024-11-11 23:42:30 -0500

    Prifile

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,16 +2,14 @@ const ApiError = require("../error/ApiError");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { User, Basket } = require("../models/models");
-const generateJwt=(id,email,role)=>{
- return   jwt.sign(
-        { id, email, role },
-        process.env.SECRET_KEY,
-        { expiresIn: "24h" }
-      );
-}
+const generateJwt = (id, email, role) => {
+  return jwt.sign({ id, email, role }, process.env.SECRET_KEY, {
+    expiresIn: "24h",
+  });
+};
 class UserController {
   async registration(req, res, next) {
-    console.log("USER REGISTRATION")
+    console.log("USER REGISTRATION");
     const { email, password, role } = req.body;
     if (!email || !password) {
       return next(ApiError.badRequest("Bad password or bad email"));
@@ -24,35 +22,34 @@ class UserController {
     const hashPassword = await bcrypt.hash(password, 5);
     const user = await User.create({ email, role, password: hashPassword });
     // const basket = await Basket.create({ userId: user.id });
-    console.log("USER REGISTRATION22222222")
-    const token = generateJwt(user.id,email,user.role)
+    console.log("USER REGISTRATION22222222");
+    const token = generateJwt(user.id, email, user.role);
     return res.json({ token });
   }
-  async login(req, res,next) {
-    const {email,password} =req.body
-    const user =await User.findOne({where:{email}})
-    
-    // console.log("USER",user)
-    if(!user){
-        return next(ApiError.internal('User with this name is not find'))
-    }
-    console.log("USER EXIST")
-    let comparePassword=bcrypt.compareSync(password, user.password)
-    if(!comparePassword){
-        return next(ApiError.internal('User with this password is not find'))
-        // return res.json({token})
+  async login(req, res, next) {
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email } });
 
+    // console.log("USER",user)
+    if (!user) {
+      return next(ApiError.internal("User with this name is not find"));
     }
-    const token =generateJwt(user.id,user.email,user.role)
-    return res.json({token})
+    console.log("USER EXIST");
+    let comparePassword = bcrypt.compareSync(password, user.password);
+    if (!comparePassword) {
+      return next(ApiError.internal("User with this password is not find"));
+      // return res.json({token})
+    }
+    const token = generateJwt(user.id, user.email, user.role);
+    return res.json({ token });
   }
   async check(req, res, next) {
     // console.log("CHECK")
     //  res.json({message:"WORaaaKING"});
     ///funktsia check generiruet novyi token i otpravit token na client////////
     ///Grubo govoria esli polzovatel budet ispolzovat svoi account token u nego budet perezapisyvatsia//////////
-    const token =generateJwt(req.user.id,req.user.email,req.user.role)
-    return res.json({token})
+    const token = generateJwt(req.user.id, req.user.email, req.user.role);
+    return res.json({ token });
     /////////////////////////////////
     // res.json("WORKING WITH CONTROLLERS");
     // const query = req.query
@@ -64,6 +61,52 @@ class UserController {
     // }
     // res.json(id);
   }
-}
 
+  ///////UDATE/////////
+  async update(req, res, next) {
+    try {
+      const {
+        userId,
+        email,
+        password,
+        userName,
+        firstName,
+        middleName,
+        lastName,
+        country,
+        region,
+        city,
+        streetAddress,
+        postalCode,
+        phoneNumber,
+      } = req.body;
+console.log("req.body",req.body)
+      const user = await User.update(
+        {
+          email,
+          password,
+          userName,
+          firstName,
+          middleName,
+          lastName,
+          country,
+          region,
+          city,
+          streetAddress,
+          postalCode,
+          phoneNumber,
+        },
+        {
+          where: {
+            id: userId,
+          },
+        }
+      );
+      //posle sozdania device peredaem informatsiu na klienta
+      return res.json(user);
+    } catch (e) {
+      next(ApiError.badRequest(e.message));
+    }
+  }
+}
 module.exports = new UserController();
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -5,6 +5,7 @@ const authMiddleware = require('../middleware/authMiddleware');
 router.post('/registration', userController.registration);
 router.post('/login', userController.login);
 router.get('/auth', authMiddleware, userController.check);
+router.put('/',  userController.update);
 // router.get('/auth', userController.check);
 // router.get('/auth',(req,res)=>{
 //     res.json({message:'ALL WORKING'})
